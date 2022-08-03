7f8e4b4b8ab799e8cd6973758b4175b5d6aaa899
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-03 21:00:34 +0500

    done

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,6 +3,8 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
-        System.out.println("java \n is \n smth");
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
     }
 }
